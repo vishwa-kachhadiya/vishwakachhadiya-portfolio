1c4325082ac3b1597914adc1f478166a8035b859
vishwa-kachhadiya <vishwa.kachhadiyaa@gmail.com>
2026-08-17 23:31:18 -0400

    Updated portfolio

--- vishwakachhadiyaportfolio-main/src/components/VideoIntro.tsx
+++ vishwakachhadiyaportfolio-main/src/components/VideoIntro.tsx
@@ -228,7 +228,7 @@ export default function VideoIntro() {
           <a href="#publications">Publications</a>
           <a href="#experience">Experience</a>
           <a href="#projects">Projects</a>
-          <a href="#toolbox">Skill</a>
+          <a href="#skills">Skills</a>
           <a href="#contact">Contact</a>
         </div>
       </nav>
