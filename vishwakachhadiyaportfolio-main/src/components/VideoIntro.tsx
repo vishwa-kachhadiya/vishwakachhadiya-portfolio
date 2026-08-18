9fb20abfbed797f88b360d607ea5388ee3e3b24a
vishwa-kachhadiya <vishwa.kachhadiyaa@gmail.com>
2026-08-17 22:32:42 -0400

    Added portfolio navigation bar

--- vishwakachhadiyaportfolio-main/src/components/VideoIntro.tsx
+++ vishwakachhadiyaportfolio-main/src/components/VideoIntro.tsx
@@ -208,9 +208,10 @@ export default function VideoIntro() {
             <div className={styles.navLinks}>
               <a href="#home">Home</a>
               <a href="#about">About</a>
-              <a href="#projects">Projects</a>
-              <a href="#publications">Publications</a>
               <a href="#experience">Experience</a>
+              <a href="#publications">Publications</a>
+              <a href="#projects">Projects</a>
+              <a href="#toolbox">Skill</a>
               <a href="#contact">Contact</a>
             </div>
           </nav>
